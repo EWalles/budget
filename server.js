f11f1b1e69ca2d0b68b2014c66c908ada49e75de
EWalles <ejomcdonald@gmail.com>
2022-08-31 18:35:00 -0400

    setting up server.js including comments to identify where the code needs to go

---
+++ server.js
@@ -0,0 +1,25 @@
+//require dependencies
+const express  = require('express');
+const budget = require(".budgets")
+
+//initialize express app
+const app = express();
+const port = 3000
+
+//middleware
+
+
+//define routes
+
+//index
+
+//new
+
+// d
+// u
+// c
+// e
+
+// show
+
+//port listen
