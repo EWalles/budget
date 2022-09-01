897b422af2291ac55e73571fe1c19b8dc5771303
EWalles <ejomcdonald@gmail.com>
2022-09-01 07:47:59 -0400

    added to server.js

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 //require dependencies
-const express  = require('express');
-const budget = require(".budgets")
+const express = require("express");
+const budget = require("./models/budget");
+const bodyParser = require("body-parser")
+
 
 //initialize express app
 const app = express();
 const port = 3000
 
+//bank
+let bankAcc = 0
+
 //middleware
+app.use(express.static("Public"))
+app.use(express.urlencoded({ extended: false }))
+app.get("/budgets", (req, res) => {
+    res.render("index.ejs", {
+        data: budget,
+        money: bankAcc,
+    })
+})
 
 
 //define routes
@@ -19,17 +32,31 @@ app.get("/budgets/:index", (req, res) =>{
 app.get("/budgets/new", (req, res) =>{
     res.send("")
 })
-//index
+// new
+app.get("/budgets/new", (req, res) => {
+    res.render("new.ejs", {
+        newData: budget,
+    })
+})
 
-//new
-
-// d
-// u
-// c
-// e
+// create
+app.post("/budgets", (req, res) => {
+    // separate tags data to an array
+    let tags = req.body.tags.split(',');
+    // set tags in body to an array
+    req.body.tags = tags;
+    budget.push(req.body)
+    // console.log(budget)
+    res.redirect("/budgets")
+})
 
 // show
-app.post("/budgets", (req, res) => {
-    res.send("")
+app.get("/budgets/:index", (req, res) => {
+    res.render("show.ejs", {
+        dataIndex: budget[req.params.index],
+    })
 })
-//port listen
+//listen
+app.listen(port, () => {
+    console.log("Chicago is listening")
+});
